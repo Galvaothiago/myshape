37d2ba084e33dc483b6455bf8009abd85226cfd6
thiago <thiagogalvaodev@outlook.com>
2023-11-20 17:07:45 -0300

    added react navigation libs and expo dependencies

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,24 @@
+import { createStackNavigator } from "@react-navigation/stack"
+import { FirstAccess } from "../screens/FirstAccess";
+import { Home } from "../screens/Home";
+
+const {Screen, Navigator} = createStackNavigator();
+
+export const StackRoutes = () => {
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Screen
+                name="firstAccess"
+                component={FirstAccess}
+            />
+            <Screen 
+                name="home"
+                component={Home}
+            />
+        </Navigator>
+    )
+}
